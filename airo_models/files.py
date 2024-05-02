f9c34cd4c1b6e5d7756d6e0b706d6edd6fe16774
Andreas <andreas.verleysen@gmail.com>
2024-05-02 17:15:33 +0200

    add schunk_egk40 with and without fingertips and add to files.py

--- airo_models/files.py
+++ airo_models/files.py
@@ -19,6 +19,10 @@ def get_urdf_path(name: str) -> str:
         "ur3e": os.path.join(airo_models_dir, "arms", "ur3e", "ur3e.urdf"),
         "ur5e": os.path.join(airo_models_dir, "arms", "ur5e", "ur5e.urdf"),
         "robotiq_2f_85": os.path.join(airo_models_dir, "grippers", "robotiq_2f_85", "urdf", "robotiq_2f_85.urdf"),
+        "schunk_egk40": os.path.join(airo_models_dir, "grippers", "schunk_egk40", "urdf", "schunk_egk40.urdf"),
+        "schunk_egk40_magneto": os.path.join(
+            airo_models_dir, "grippers", "schunk_egk40_magneto", "schunk_egk40_magneto.urdf"
+        ),
         "zed2i": os.path.join(airo_models_dir, "cameras", "zed2i", "zed2i.urdf"),
         "d435": os.path.join(airo_models_dir, "cameras", "realsense", "d435", "d435.urdf"),
         "table8080": os.path.join(airo_models_dir, "environment", "tables", "table8080.urdf"),
